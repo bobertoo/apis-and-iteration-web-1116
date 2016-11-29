8d0114172ca3081b216e1383c743f22644f818a9
Ashley Robinson <ashleyhowerobinson@gmail.com>
2016-11-29 15:26:54 -0500

    Goes through all the pages until it finds the right character

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,28 +4,44 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  the_chosen_one = character_hash["results"].find { |character_search| character_search["name"].downcase == character }
-  the_chosen_one["films"]
-end
+  url = 'http://www.swapi.co/api/people/'
+  the_chosen_one = nil
+  character_hash = jsonify(url)
 
-def parse_character_movies(films_hash)
-  films_hash.collect do |film_url|
-
-    film_stuff = RestClient.get(film_url)
-    film_json = JSON.parse(film_stuff)
-
-    puts film_json["title"]
-    film_json["title"]
+  while character_hash["next"] && !the_chosen_one do
+    next_hash = character_hash["next"]
+    the_chosen_one = character_hash["results"].find { |character_search| character_search["name"].downcase == character }
+    character_hash = jsonify(next_hash)
   end
 
-  # some iteration magic and puts out the movies in a nice list
+  if the_chosen_one == nil
+    []
+  else
+    the_chosen_one["films"]
+  end
+end
+
+def jsonify(url)
+  hash = RestClient.get(url);
+  JSON.parse(hash)
+end
+
+
+def parse_character_movies(films_hash)
+  # binding.pry
+  films_hash.collect do |film_url|
+    jsonify(film_url)["title"]
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  # binding.pry
+  if parse_character_movies(films_hash).length > 0
+    puts parse_character_movies(films_hash)
+  else
+    puts "Bad luck"
+  end
 end
 
 ## BONUS
